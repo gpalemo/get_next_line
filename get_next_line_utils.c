aeeff98470b39575800ca5af577da9a80881d3f5
gpalemo <chloemauley@gmail.com>
2025-11-15 18:53:13 +0100

    passe pas en strict ptn

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: cmauley <cmauley@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 16:13:25 by cmauley           #+#    #+#             */
-/*   Updated: 2025/11/15 18:28:12 by cmauley          ###   ########.fr       */
+/*   Updated: 2025/11/15 18:47:47 by cmauley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlen(const char *str)
 
 	i = 0;
 	if (!str)
-		return(0);
+		return (0);
 	while (str && str[i])
 	{
 		i++;
@@ -26,26 +26,26 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-char *gnl_strjoin(char const *s1, char const *s2)
+char	*gnl_strjoin(char const *s1, char const *s2)
 {
-	char *str;
-	int	len1;
-	int len2;
-	int i;
-	
+	char	*str;
+	int		len1;
+	int		len2;
+	int		i;
+
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	i = 0;
 	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!str)
-		return(NULL);
+		return (NULL);
 	while (i < len1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
 	i = 0;
-	while(i < len2)
+	while (i < len2)
 	{
 		str[i + len1] = s2[i];
 		i++;
@@ -61,10 +61,11 @@ char	*free_wrapper(char *str)
 	str = NULL;
 	return (NULL);
 }
+
 char	*setup_stash(int fd, char *stash)
 {
 	ssize_t		lu;
-	char	*buff;
+	char		*buff;
 
 	buff = malloc(BUFFER_SIZE + 1);
 	if (!buff)
@@ -74,7 +75,7 @@ char	*setup_stash(int fd, char *stash)
 	{
 		lu = read(fd, buff, BUFFER_SIZE);
 		if (lu <= 0)
-			break;
+			break ;
 		buff[lu] = '\0';
 		stash = gnl_strjoin(stash, buff);
 		if (!stash)
